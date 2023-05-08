0d61bc79af49366c8e013e2b71fc652f84b6a532
shivarajchimouriya <shivarajchimouriya@bottle.com.np>
2023-05-08 16:31:04 +0545

    updated message to 1 and 0

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,49 @@
 import "./App.css";
 import * as mqtt from "mqtt/dist/mqtt.min";
-import { useEffect } from "react";
-import { configureAbly, useChannel, } from "@ably-labs/react-hooks";
+import { useEffect, useState } from "react";
+import { configureAbly, useChannel } from "@ably-labs/react-hooks";
 function App() {
-  useEffect(() => {
-    
-  }, []);
+  const [isOn, setIsOn] = useState(false);
+
+  useEffect(() => {}, []);
   configureAbly({
-    key: 'nwRYQw.bLdR4g:-J0puQdH18uoob6EEbK1SQih0zz1USTQggYSU1EJucM',
+    key: "nwRYQw.bLdR4g:-J0puQdH18uoob6EEbK1SQih0zz1USTQggYSU1EJucM",
     clientId: Math.random().toString(36).substring(7),
   });
-  const [channel] = useChannel("your-channel-name", "test-message", (message) => {
-    console.log(message); 
-    // Only logs messages sent using the `test-message` message type
-});
-const handleClick = () => {
-
-  channel.publish("test-message", { text: "message text" });
-
-}
+  const [channel] = useChannel(
+    "your-channel-name",
+    "test-message",
+    (message) => {
+      console.log(message);
+      // Only logs messages sent using the `test-message` message type
+    }
+  );
+  const handleClick = () => {
+    const data = isOn ? 0 : 1;
+    channel.publish("data",`${data}`);
+    setIsOn((isOn) => !isOn);
+  };
   return (
     <div className="w-full h-screen bg-slate-900 flex justify-center items-center">
-      <button  onClick={handleClick}  className="bg-red-500 text-white px-4 py-2 rounded-md">
-      
-        CLICK ME
+      <button
+        onClick={handleClick}
+        className="bg-red-500 text-white px-4 py-2 rounded-md"
+        style={{
+          boxShadow: `  ${
+            isOn
+              ? ` inset 0 0 50px #fff,
+    inset 20px 0 80px #f0f,
+    inset -20px 0 80px #0ff,
+    inset 20px 0 300px #f0f,
+    inset -20px 0 300px #0ff,
+    0 0 50px #fff,
+    -10px 0 80px #f0f,
+    10px 0 80px #0ff`
+              : ""
+          }`,
+        }}
+      >
+        {isOn ? "turn off" : "turn on"}
       </button>
     </div>
   );
